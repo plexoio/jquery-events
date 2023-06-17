80b7c57e107cc123f32d747b04d292a766e563f1
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-17 13:58:39 +0000

    get to know methods for DOM interacction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+    $("#stream1_btn").click(function() { // does the same as .on('click', function() {}); call-back
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
    });
-   $("#stream2_btn").on("click", function() {
+   $("#stream2_btn").mouseenter(function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
+   }).mouseleave(function(){ // start chaining
+     $(".stream2").removeClass('highlight_stream');
    });
    $("#stream3_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
